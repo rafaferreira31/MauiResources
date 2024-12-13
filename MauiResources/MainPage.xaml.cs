8bc37ec8d7503ac7726a2a18a4822165c8c72189
Rafael Ferreira <Rafael.Ferreira.35139@formandos.cinel.pt>
2024-12-13 15:07:13 +0000

    Add project files.

---
+++ MauiResources/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json;
+
+namespace MauiResources
+{
+    public partial class MainPage : ContentPage
+    {
+        Pessoa pessoa;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing(); 
+
+            await LoadMauiAsset();
+        }
+
+        async Task LoadMauiAsset()
+        {
+            using var stream = await FileSystem.OpenAppPackageFileAsync("demo.json");
+            using var reader = new StreamReader(stream);
+
+            var contents = reader.ReadToEnd();
+
+            var pessoa = JsonSerializer.Deserialize<Pessoa>(contents);
+
+            BindingContext = pessoa;
+        }
+    }
+
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public int Idade { get; set; }
+
+        public string Foto { get; set; }
+
+    }
+
+}
+
+
